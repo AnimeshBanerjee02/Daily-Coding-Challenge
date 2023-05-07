93d909bb3aebb2f71fa54870d7625fc344f286d7
Animesh Banerjee <113301447+AnimeshBanerjee02@users.noreply.github.com>
2023-05-07 09:39:02 +0530

    Add files via upload

---
+++ Inver Binary Tree.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        if (!root) {
+            return NULL;
+        }
+        TreeNode* left = invertTree(root->left);
+        TreeNode* right = invertTree(root->right);
+        root->left = right;
+        root->right = left;
+        return root;
+    }
+};
