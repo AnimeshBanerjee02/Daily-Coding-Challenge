5a5b1d3f1bf32ee126beb3a4bc96081c3d8d2ac7
Animesh Banerjee <113301447+AnimeshBanerjee02@users.noreply.github.com>
2023-05-23 20:12:41 +0530

    Add files via upload

---
+++ Kth Largest Element in a Stream.py
@@ -0,0 +1,14 @@
+import heapq
+
+class KthLargest:
+    def __init__(self, k: int, nums: List[int]):
+        self.k = k
+        self.heap = []
+        for num in nums:
+            self.add(num)
+
+    def add(self, val: int) -> int:
+        heapq.heappush(self.heap, val)
+        if len(self.heap) > self.k:
+            heapq.heappop(self.heap)
+        return self.heap[0]
